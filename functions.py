dd8dfef8625500d92950a3c871f46b7fa72aa6c9
afan104 <amyfan061@gmail.com>
2024-06-25 21:37:28 -0700

    custom dataset and computer vision

--- functions.py
+++ functions.py
@@ -1,7 +1,32 @@
 import torch
+from timeit import default_timer as timer
 
 
+# get device
 def get_device():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     device = "mps" if torch.backends.mps.is_available() else device
     return device
+
+
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_pred, y_true):
+    """Calculates accuracy between truth labels and predictions.
+
+    Args:
+        y_true (torch.Tensor): Truth labels for predictions.
+        y_pred (torch.Tensor): Predictions to be compared to predictions.
+
+    Returns:
+        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+    """
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+def print_train_time(start: float, end: float, device: torch.device = None):
+    """Prints difference between start and end time."""
+    total_time = end - start
+    print(f"Training time on {device}: {total_time:.4f} seconds")
+    return total_time
